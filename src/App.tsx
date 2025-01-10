48f7d8a78eb04174e32a1ea53006c909081975db
romashka-dev <romikkbox@gmail.com>
2025-01-10 07:34:42 +0200

    [chore] Add some dependencies for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import './App.css'
 
 function App() {
